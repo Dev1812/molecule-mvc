537980737444db65559225884ceeabdac6544f4f
Timur <timka_issaev@mail.ru>
2021-03-19 12:07:23 +0300

    file was improved

--- application/core/view.php
+++ application/core/view.php
@@ -10,15 +10,6 @@ class View {
      *
      */
   public function generate($content_view, $template_view, $param, $data = null, $i18n = null) {
-   /*if(User::isAuth()) {
-      $user = new User;
-      $user_info = $user->getInfo(intval($_SESSION['user_id']), 'firstname, lastname, small_photo, big_photo');
-      $user_info['initials'] = $user_info['firstname'].' '.$user_info['firstname'];
-      $no_photo = '/images/no_photo.png';
-      $user_info['small_photo'] = (!empty($user_info['small_photo'])) ? $user_info['small_photo'] : $no_photo;
-      $user_info['big_photo'] = (!empty($user_info['big_photo'])) ? $user_info['big_photo'] : $no_photo;
-      $data['global_info'] = $user_info;
-    }*/
     if(!isset($template_view)) { 
       if(isset($content_view)) {
         include 'application/views/'.$content_view;
