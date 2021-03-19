a4656c8933025698a443780bbf57d128afb68bc4
Timur <timka_issaev@mail.ru>
2021-03-19 12:01:13 +0300

    added some files

---
+++ application/bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+  require_once SITE_ROOT.'application/core/view.php';
+  require_once SITE_ROOT.'application/core/route.php';
+  require_once SITE_ROOT.'application/core/controller.php';
+  Route::start();
+?>
